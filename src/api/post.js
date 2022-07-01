ca98c3e43ad5edb6beaacb7ed726435d5151a431
Nik-McKnight <nik.mcknight735@gmail.com>
2022-07-01 14:20:41 -0600

    Fetch all posts now works

--- src/api/post.js
+++ src/api/post.js
@@ -1,5 +1,7 @@
-import React from "react";
-
-export default function post() {
-  return <div></div>;
-}
+export const fetchAllPosts = async () => {
+  const response = await fetch(
+    "https://strangers-things.herokuapp.com/api/2206-ftb-mt-web-ft/posts"
+  );
+  const result = await response.json();
+  return result;
+};
--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,5 +1,26 @@
-import React from "react";
+import { fetchAllPosts } from "api/post";
+import React, { useEffect, useState } from "react";
 
 export default function Posts() {
-  return <div></div>;
+  const [postList, setPostList] = useState([]);
+
+  useEffect(() => {
+    const getAllPosts = async () => {
+      const results = await fetchAllPosts();
+      console.log("results:", results);
+      setPostList(results.data.posts);
+    };
+    getAllPosts();
+    //console.log(postList.data.posts);
+  }, []);
+
+  console.log("postList:", postList);
+
+  return (
+    <div>
+      {postList.map((post, index) => {
+        return <div>{post.location}</div>;
+      })}
+    </div>
+  );
 }
