5163e2e73e37f7d47a17c7299e785dd712c55aeb
tomrrees <thomasrrees@gmail.com>
2022-07-01 11:26:51 -0600

    Initial phase of project, built basic components

--- src/App.js
+++ src/App.js
@@ -1,3 +1,15 @@
+import { Profile, Posts, NavBar, LogOut } from "components";
+import { Routes, Route } from "react-router-dom";
+
 export default function App() {
-  return <></>
+  return (
+    <div>
+      <NavBar />
+      <Routes>
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/" element={<Posts />} />
+        <Route path="/logout" element={<LogOut />} />
+      </Routes>
+    </div>
+  );
 }
---
+++ src/components/Index.js
@@ -0,0 +1,4 @@
+export { default as Profile } from "./Profile";
+export { default as Posts } from "./Posts";
+export { default as NavBar } from "./NavBar";
+export { default as LogOut } from "./LogOut";
